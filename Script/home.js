a5547c85e753bd1886b57261cc13820c97eae759
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-22 14:08:09 +0530

    rendered api data

--- Script/home.js
+++ Script/home.js
@@ -1,3 +1,30 @@
+async function render(obj){
+    const cont=document.querySelector(".track-result");
+    const div=document.createElement("div");
+    const c_div=document.createElement("div");
+    const img=document.createElement("img");
+    const name=document.createElement("h2");
+    const email=document.createElement("h2");
+    const location=document.createElement("h2");
+    const salary=document.createElement("h2");
+    div.appendChild(img);
+    div.appendChild(c_div);
+    c_div.appendChild(name);
+    c_div.appendChild(email);
+    c_div.appendChild(location);
+    c_div.appendChild(salary);
+    div.setAttribute("class","profile");
+    img.setAttribute("src",`${obj.image}`);
+    name.innerHTML=`Name: ${obj.name}`;
+    email.innerHTML=`Email: ${obj.email}`;
+    location.innerHTML=`Counter: ${obj.location}`;
+    salary.innerHTML=`Salary: ${obj.salary}`;
+    cont.appendChild(div);
+    console.log(div);
+}
+
+
+
 async function api_call(){
     const prof=await fetch("https://randomuser.me/api/?results=15");
     const results=await prof.json();
@@ -10,6 +37,7 @@ async function api_call(){
         obj["location"]=ele.location.country;
         obj["image"]=ele.picture.thumbnail;
         obj["salary"]=100000;
-        console.log(obj);
+        // console.log(obj);
+        render(obj);
     })
 }
