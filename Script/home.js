df96ca7fb168b8f671b92c93b12d76b01d119775
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-24 14:14:41 +0530

    added rupee symbol

--- Script/home.js
+++ Script/home.js
@@ -16,7 +16,7 @@ async function render(obj){
         <h2>${obj.name}</h2>
         <h2>${obj.email}</h2>
         <h2>${obj.location}</h2>
-        <h2>${obj.salary}</h2>
+        <h2>&#8377 ${obj.salary}</h2>
     </div>`;
     div.setAttribute("class","profile");
     cont.appendChild(div);
@@ -33,13 +33,13 @@ function add_expense(obj,data){
     let div=document.createElement("div");
     div.innerHTML=`<h2>${obj.exp}</h2> 
         <p>:</p> 
-        <span>${obj.amount}</span>
+        <span>&#8377 ${obj.amount}</span>
         <img src="images/close.png" class="close">`
     div.setAttribute("class","expense");
     salary_cont.insertBefore(div,rAmount);
     let close=div.children[3];
     data.rAmount-=obj.amount;
-    rAmount.children[2].innerHTML=data.rAmount;
+    rAmount.children[2].innerHTML=`&#8377 `+data.rAmount;
     localStorage.setItem(text,JSON.stringify(data));
     // console.log(close);
     close.addEventListener("click",()=>{
@@ -53,7 +53,7 @@ function add_expense(obj,data){
         // console.log(typeof data.rAmount);
         data.rAmount+=obj.amount;
         console.log(typeof data.rAmount);
-        rAmount.children[2].innerHTML=data.rAmount;
+        rAmount.children[2].innerHTML=`&#8377 `+data.rAmount;
         localStorage.setItem(text,JSON.stringify(data));
         parent.remove();
     })
@@ -94,8 +94,8 @@ async function api_call(){
             // console.log(data);
             let sala=document.querySelector("#salary");
             let amount=document.querySelector("#remaining-amount");
-            sala.children[2].innerHTML=data.salary;
-            amount.children[2].innerHTML=data.rAmount;
+            sala.children[2].innerHTML=`&#8377 `+data.salary;
+            amount.children[2].innerHTML=`&#8377 `+data.rAmount;
             // console.log(data.expense);
             data.expense.forEach((ele)=>{
                 add_expense(ele,data);
