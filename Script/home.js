01e6c85ff69df78921f8df626e6398f319750c15
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-24 09:19:14 +0530

    added localStorage for expense section

--- Script/home.js
+++ Script/home.js
@@ -1,3 +1,13 @@
+var add=document.querySelector(".salary-input button");
+var salary_cont=document.querySelector(".salary-cont");
+var exep=document.querySelector("#exepense");
+var amount=document.querySelector("#amount");
+var tSalary=document.querySelector(".total-salary");
+var rAmount=document.querySelector("#remaining-amount");
+var salary= document.querySelector(".salary-list") 
+
+
+
 async function render(obj){
     const cont=document.querySelector(".track-result");
     const div=document.createElement("div");
@@ -10,11 +20,46 @@ async function render(obj){
     </div>`;
     div.setAttribute("class","profile");
     cont.appendChild(div);
+    let text=div.children[1].children[1].textContent;
+    // console.log(text);
     // console.log(div);
+    if((localStorage.getItem(text)==null)){
+        let data={salary:obj.salary,expense:[],rAmount:obj.salary};
+        localStorage.setItem(text,JSON.stringify(data));
+    }
 }
 
-
-var salary= document.querySelector(".salary-list") 
+function add_expense(obj,data){
+    let div=document.createElement("div");
+    div.innerHTML=`<h2>${obj.exp}</h2> 
+        <p>:</p> 
+        <span>${obj.amount}</span>
+        <img src="images/close.png" class="close">`
+    div.setAttribute("class","expense");
+    salary_cont.insertBefore(div,rAmount);
+    let close=div.children[3];
+    data.rAmount-=obj.amount;
+    rAmount.children[2].innerHTML=data.rAmount;
+    localStorage.setItem(text,JSON.stringify(data));
+    // console.log(close);
+    close.addEventListener("click",()=>{
+        let parent=close.parentNode;
+        // console.log(parent);
+        let itx=data.expense.indexOf(obj);
+        // console.log(itx);
+        data.expense.splice(itx,1);
+        // console.log(text);
+        // console.log(data);
+        // console.log(typeof data.rAmount);
+        data.rAmount+=obj.amount;
+        console.log(typeof data.rAmount);
+        rAmount.children[2].innerHTML=data.rAmount;
+        localStorage.setItem(text,JSON.stringify(data));
+        parent.remove();
+    })
+}
+var data;
+var text;
 async function api_call(){
     const prof=await fetch("https://randomuser.me/api/?results=15");
     let results=await prof.json();
@@ -44,32 +89,27 @@ async function api_call(){
                     salary.classList.remove("salary-unactive");
                 }
             })
+            text=ele.children[1].children[1].textContent;
+            data=JSON.parse(localStorage.getItem(text));
+            // console.log(data);
+            let sala=document.querySelector("#salary");
+            let amount=document.querySelector("#remaining-amount");
+            sala.children[2].innerHTML=data.salary;
+            amount.children[2].innerHTML=data.rAmount;
+            // console.log(data.expense);
+            data.expense.forEach((ele)=>{
+                add_expense(ele,data);
+            })
         })
     })
 }
 
-var add=document.querySelector(".salary-input button");
-var salary_cont=document.querySelector(".salary-cont");
-var exep=document.querySelector("#exepense");
-var amount=document.querySelector("#amount");
-var tSalary=document.querySelector(".total-salary");
-var rAmount=document.querySelector("#remaining-amount");
+
 add.addEventListener("click",()=>{
-    let div=document.createElement("div");
-    div.innerHTML=`<h2>${exep.value}</h2> 
-        <p>:</p> 
-        <span>${amount.value}</span>
-        <img src="images/close.png" class="close">`
-    div.setAttribute("class","expense");
-    salary_cont.insertBefore(div,rAmount);
-    // var close=document.querySelector(".close");
-    let close=div.children[3];
-    // console.log(close);
-    close.addEventListener("click",()=>{
-        let parent=close.parentNode;
-        // console.log(parent);
-        parent.remove();
-    })
+    let obj={exp:exep.value,amount:parseInt(amount.value)};
+    add_expense(obj,data);
+    data.expense.push(obj);
+    localStorage.setItem(text,JSON.stringify(data));
 });
 
 
